72611c51ef3e61be97272c0fbf31c3a9b33f5bd4
Liam-Horgan <14164736@studentmail.ul.ie>
2017-03-22 16:17:55 +0000

    Update register.php

--- register.php
+++ register.php
@@ -1,18 +1,21 @@
 <?php
-ob_start();
+//ob_start();
 session_start();
-if( isset($_SESSION['user'])!="" ){
-	header("Location: dashbord.php");
-}
-include_once 'config.php';
+ include('config.php');
+ 
+ if ( isset($_SESSION['userId'])!="" ) {
+  header("Location: dashboard.php");
+  exit;
+ }
 $error = false;
-if ( isset($_POST['btn-signup']) ) {
-
+ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+	 
 	// clean user inputs to prevent sql injections
+	
 	$firstname = trim($_POST['firstname']);
 	$firstname = strip_tags($firstname);
 	$firstname = htmlspecialchars($firstname);
-
+	
 	$lastname = trim($_POST['lastname']);
 	$lastname = strip_tags($lastname);
 	$lastname = htmlspecialchars($lastname);
@@ -20,14 +23,14 @@ if ( isset($_POST['btn-signup']) ) {
 	$email = trim($_POST['email']);
 	$email = strip_tags($email);
 	$email = htmlspecialchars($email);
-
-	$pass = trim($_POST['pass']);
-	$pass = strip_tags($pass);
-	$pass = htmlspecialchars($pass);
-
-	$studentID = trim($_POST['studentID']);
-	$studentID = strip_tags($studentID);
-	$studentID = htmlspecialchars($studentID);
+	
+	$pwd = trim($_POST['pwd']);
+	$pwd = strip_tags($pwd);
+	$pwd = htmlspecialchars($pwd);
+	
+	$id = trim($_POST['id']);
+	$id = strip_tags($id);
+	$id = htmlspecialchars($id);
 	
 	// email validation
 	if ( !filter_var($email,FILTER_VALIDATE_EMAIL) ) {
@@ -35,57 +38,142 @@ if ( isset($_POST['btn-signup']) ) {
 		$emailError = "Please enter valid email address.";
 	} else {
 		//  email exist or not
-		$query = "SELECT userEmail FROM users WHERE userEmail='$email'";
-		$result = mysql_query($query);
-		$count = mysql_num_rows($result);
-		if($count!=0){
-			$error = true;
-			$emailError = "Provided Email is already in use.";
-		}
+		$sql = "SELECT userEmail FROM users WHERE userEmail='$email'";
+		$result = mysqli_query($db,$sql);
+		$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+		$count = mysqli_num_rows($result);
+      
+		// If result matched $myusername and $mypassword, table row must be 1 row
+		
+		if($count == 1) {
+        
+			$_SESSION['userId'] = $row["userId"];
+         
+			header("location: dashboard.php");
+		}else {
+         $error = true;
+      }
 	}
 	
 	//studentID val
-	if(empty($studentID)){
+	if(empty($id)){
 		$error=true;
 		$studentIDError="Please enter youur student ID.";
 	}
-	else if(strlen($studentID)!=8){
+	else if(strlen($id)!=8){
 		$error=true;
 		$studentIDError="Must be 8 characters long.";
 	}
-	else if(!preg_match("/^(?:0|[1-9][0-9]*)$/",$studentID)){
-		$studentID=true;
+	else if(!preg_match("/^(?:0|[1-9][0-9]*)$/",$id)){
+		$id=true;
 		$studentIDError="May only contain numbers.";
 	}
 	// password validation
-	if (empty($pass)){
+	if (empty($pwd)){
 		$error = true;
 		$passError = "Please enter password.";
-	} else if(strlen($pass) < 6) {
+	} else if(strlen($pwd) < 6) {
 		$error = true;
 		$passError = "Password must have atleast 6 characters.";
 	}
-
 	// password encrypt using SHA256();
-	$password = hash('sha256', $pass);
+	$password = hash('sha256', $pwd);
 	if( !$error ) {
-		$query = "INSERT INTO users(firstName,lastName,studentId,userEmail,userPass) VALUES('$firstname','$lastname','$studentID','$email','$password')";
+		$query = "INSERT INTO users(firstName,lastName,id,userEmail,userPass) VALUES('$firstname','$lastname','$id','$email','$pwd')";
 		$res = mysql_query($query);
-
 		if ($res) {
 			$errTyp = "success";
 			$errMSG = "Successfully registered, you may login now";
 			unset($firstname);
 			unset($lastname);
-			unset($studentID);
+			unset($id);
 			unset($email);
-			unset($pass);
+			unset($pwd);
 		} else {
 			$errTyp = "danger";
 			$errMSG = "Something went wrong, try again later...";
 		}
-
 	}
-}
-?>
-<?php ob_end_flush(); ?>
+} ?>
+
+<html lang="en"><head>
+    <meta charset="utf-8">
+
+
+    <title>Register - Proofreaders</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS: You can use this stylesheet to override any Bootstrap styles and/or apply your own styles -->
+    <link href="css/custom.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+        <div class="container">
+            <!-- Logo-->
+            <div class="navbar-header">
+                <a class="navbar-brand" href="index.html"><span class="glyphicon glyphicon-education"></span> Proofreaders</a>
+            </div>
+        </div>
+        <!-- /.container -->
+    </nav>
+
+<div class="container-fluid">
+
+	
+	  <div class="col-center">
+
+			<!-- Form --> 
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">
+						<span class="glyphicon glyphicon-log-in"></span> 
+						Register an account
+					</h3>
+				</div>
+				<div class="panel-body">
+					<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+						<div class="form-group">
+							<input type="text" class="form-control" id="email" name="email" placeholder="Email">
+						</div>
+						<div class="form-group">
+							<input type="text" class="form-control" id="firstname" name="firstname" placeholder="First Name">
+						</div>
+						<div class="form-group">
+							<input type="text" class="form-control" id="lastname" name="lastname" placeholder="Last Name">
+						</div>
+						<div class="form-group">
+							<input type="text" class="form-control" id="id" name="id" placeholder="Student/Staff ID">
+						</div>
+						<div class="form-group">
+						<p>Select a discipline</p>
+							<select class="form-control" name="discipline" id="discipline">
+								<option value="one">Education and Health Sciences</option>
+								<option value="two">Arts and Humanities</option>
+								<option value="three">Science and Engineering</option>
+								<option value="four">Business</option>
+							</select>
+						</div>			
+						<div class="form-group">
+							<input type="password" class="form-control" id="pwd" name="pwd" placeholder="Password">
+						</div>
+						<div class="form-group">
+							<input type="password" class="form-control" id="pwd2" name="pwd2" placeholder="Re-enter Password">
+						</div>
+						<button type="submit" class="btn btn-default">Register</button>
+					</form>
+					
+				</div>
+			</div>
+			
+	  </div>
+
+	</div><!--/container-fluid-->
+	</body>
+</html>
